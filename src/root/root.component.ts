75071544f019caef974977c75f167074c7b87583
Jason Calalang <jcalalang@ph.ibm.com>
2018-03-22 14:39:57 -0500

    bootstrap changes

--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -4,8 +4,6 @@ import { Component } from '@angular/core';
   selector: 'root',
   template: `
     <header-nav></header-nav>
-    <h1>hello world!</h1>
-    <button mat-raised-button color="primary" rel="hi-button">Hi</button>
     <tile></tile>
   `,
     styles: [`
